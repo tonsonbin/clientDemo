43e096c804e391c1dbd01bcc18087373381a3562
unknown <tb@git.com>
2021-05-23 18:33:44 +0800

    remarks

--- remoteTo.bat
+++ remoteTo.bat
@@ -1,9 +1,9 @@
 @echo off
 set who=%~1
 if "%who%" == "github" (
- git remote set-url origin git@github.com:tonsonbin/baseapi.git
+ git remote set-url origin git@github.com:tonsonbin/clientDemo.git
 ) ^
 else if "%who%" == "aliyun" (
- git remote set-url origin git@code.aliyun.com:tangbin/baseApiC.git
+ git remote set-url origin git@code.aliyun.com:tangbin/clientDemo.git
 )
 git remote -v
